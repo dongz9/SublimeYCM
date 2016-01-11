8c3ccf82b92631e713e19beac46c62cc864bbf30
Dong Zhou <dong.zhou.08@gmail.com>
2016-01-10 22:50:05 -0500

    remove hello_world.py, add find_unused_port

--- SublimeYCM.py
+++ SublimeYCM.py
@@ -1,5 +1,18 @@
 import sublime, sublime_plugin
 import os
+import socket
+
+def find_unused_port():
+    sock = socket.socket()
+    sock.bind(("",0))
+    port = sock.getsockname()[1]
+    sock.close()
+    return port
+
+class YcmdServer:
+    @classmethod
+    def start_ycmd():
+        return None
 
 def plugin_loaded():
     print("SublimeYCM loaded, ST version = %s" % sublime.version())
@@ -8,6 +21,12 @@ def plugin_loaded():
     print("python_executable = %s" % python_executable)
     global_extra_config = settings.get("global_extra_config")
     print("global_extra_config = %s" % global_extra_config)
+    port = find_unused_port()
+    print("port = %d" % port)
 
 def plugin_unloaded():
     print("SublimeYCM unloaded")
+
+class ExampleCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.insert(edit, 0, "Hello, World!")
